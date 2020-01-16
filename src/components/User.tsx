ed1fecca0bf5fa3201cdd4bcd785dbf7e78bed74
matthernandez <matt.hernandez@idean.com>
2020-01-16 14:39:01 -0600

    Populate Users page with data

---
+++ src/components/User.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { IonItem, IonLabel, IonButton } from '@ionic/react';
+import { ReactComponent as UserPic } from '../assets/user-pic.svg';
+import LoadingBlock from './LoadingBlock';
+import FlexRow from './FlexRow';
+
+const LabelWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  margin-left: 10px;
+`;
+
+type Props = {
+  name: string;
+  onClick?: (...args: any[]) => void;
+};
+
+const User: React.FunctionComponent<Props> = function ({ name, onClick }) {
+  return (
+    <IonItem button={true} onClick={onClick}>
+      <UserPic />
+      <LabelWrapper>
+        <IonLabel>{name}</IonLabel>
+        <IonButton>Details</IonButton>
+      </LabelWrapper>
+    </IonItem>
+  );
+}
+
+export default User;
+
+const ItemIconLoading = styled(LoadingBlock)`
+  flex-basis: 24px;
+  flex-grow: 0;
+  flex-shrink: 0;
+  width: 24px;
+  height: 24px;
+  margin-top: 14px;
+  margin-right: 14px;
+  margin-bottom: 14px;
+`;
+
+const ItemTextLoading = styled(LoadingBlock)`
+  flex-basis: 100%;
+  flex-grow: 1;
+  flex-shrink: 0;
+  height: 24px;
+  margin-right: 20px;
+`;
+
+export const UserLoading = () => (
+  <IonItem>
+    <FlexRow alignItems="center">
+      <ItemIconLoading />
+      <ItemTextLoading />
+    </FlexRow>
+  </IonItem>
+);
--- src/pages/Admin/Users.tsx
+++ src/pages/Admin/Users.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import { addPageData } from '../../util/add-page-data';
+import { withRouter } from 'react-router';
+import { USERS } from '../../apollo-client/query/admin';
+import { User as UserInterface } from '../../apollo-client/types/user';
+import User, { UserLoading } from '../../components/User';
+import useQueryHelper from '../../util/use-query-helper';
 
 const slug = '/users';
 const title = 'Users';
 
-export default addPageData(() => {
-  return <div />
-}, { slug, title });
+export default addPageData(withRouter(() => {
+  const { loading, error, data: users } = useQueryHelper<UserInterface[]>(USERS, 'users');
+  if (loading) {
+    return (
+      <>
+        <UserLoading />
+        <UserLoading />
+        <UserLoading />
+        <UserLoading />
+        <UserLoading />
+      </>
+    );
+  }
+  return (
+    <>
+      {users.map(({ name, cid }) => <User key={cid} name={name} />)}
+    </>
+  );
+}), { slug, title });
