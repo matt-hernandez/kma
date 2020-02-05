0281bc7850dfe3bfbb5b6d75a6722bde42f1c7b9
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2020-02-05 15:37:47 -0600

    Add scripts to download schema

---
+++ apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'kma',
+      url: 'http://localhost:4000',
+      // optional disable SSL validation check
+      skipSSLValidation: true
+    }
+  }
+};
