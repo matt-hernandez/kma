ab643816f31bb931b384bb6e944500aa76e06230
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2020-02-05 16:14:46 -0600

    Integrate graphql validation for gql tagged template literals

---
+++ validate-client.ts
@@ -0,0 +1,26 @@
+import { validate } from 'graphql/validation';
+import { buildSchema, GraphQLError } from 'graphql';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { red, bold } from 'colors/safe';
+import * as queries from './src/apollo-client/queries';
+import * as mutations from './src/apollo-client/mutations';
+
+const schema = readFileSync(resolve(__dirname, 'schema.graphql'), 'utf8');
+const graphQLSchema = buildSchema(schema);
+const queryErrors = Object.keys(queries).reduce((acc, name) => {
+  const ast = (queries as any)[name];
+  const errors = validate(graphQLSchema, ast);
+  return [ ...acc, ...errors ];
+}, [] as GraphQLError[]);
+const mutationErrors = Object.keys(mutations).reduce((acc, name) => {
+  const ast = (mutations as any)[name];
+  const errors = validate(graphQLSchema, ast);
+  return [ ...acc, ...errors ];
+}, [] as GraphQLError[]);
+if (queryErrors.length || mutationErrors.length) {
+  [ ...queryErrors, ...mutationErrors ].forEach((error, index) => {
+    console.error(`${index + 1}: ${bold(red(error.message))}`);
+  });
+  throw new Error('Validation failed!');
+}
